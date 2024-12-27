431ad8c749110622fdcb0edadeb4fa8c03a7b45b
ajaqueduarte <113454830+ajaqueduarte@users.noreply.github.com>
2024-12-26 21:44:38 -0500

    Changes2

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ function Home() {
   return (
     <StyledHome data-testid="container">
       <div className="home-container">
-        {bunnyState === "yes" ? <div className="title"> See you on Monday!!!!</div> : <div className="title">Hey Bonito! Do you want to hang out?</div>}
+        {bunnyState === "yes" ? <div className="title"> See you there!!!!</div> : <div className="title">Hey, do you want to go on a date with me?</div>}
         <div className="animation">
           {bunnyState === "normal" && <Lottie options={bunnyPleaseOptions} height={300} width={300} />}
           {bunnyState === "cry" && <Lottie options={bunnyCryOptions} height={300} width={300} />}
